e8c7d26f7061941babc362f436a71ca4a4e0c1e6
Jankin Wan <ichangting@126.com>
2018-01-21 20:25:10 +0800

    Create Exercise 6.8（1）.py

---
+++ Exercise 6.8（1）.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 21 19:33:43 2018
+
+@author: ichangting
+"""
+'''
+The greatest common divisor (GCD) of a and b is the largest number that divides
+both of them with no remainder.
+One way to find the GCD of two numbers is Euclid’s algorithm, which is based on the observation
+that if r is the remainder when a is divided by b, then gcd(a, b) = gcd(b, r). 
+As a base case, we can use gcd(a, 0) = a.
+'''
+
+# Exercise 6.8
+
+def gcd(a,b):
+    while b!=0:  
+        r=a%b
+        a=b
+        b=r # g(a,b)=gcd(b,r)
+    return a
+
+a=int(raw_input('a='))
+b=int(raw_input('b='))
+
+if a<b:
+    t=a
+    a=b
+    b=t 
+
+print 'a=',a
+print 'b=',b
+
+g=gcd(a,b)
+print 'The greatest common divisor is',g

5d3b7f69e9470b7a57cbe99ab2c969d6b4c148d6
Jankin Wan <ichangting@126.com>
2018-01-31 10:21:39 +0800

    Update sort.py
    新增冒泡排序法

--- sort.py
+++ sort.py
@@ -24,8 +24,25 @@ def selction_sort_v2(lst):
 # 冒泡排序
 # 与选择排序类似，但是每次遍历不止交换一次，每次遍历，将最大的值排在最后。
 
+def swap(lst,i,j):
+  temp=lst[i]
+  lst[i]=lst[j]
+  lst[j]=temp
 def bubble_sort(lst):
-  for i in range(len(lst)-1):
+    top = len(lst) - 1
+    is_exchanged = True
+
+    while is_exchanged:
+        is_exchanged = False
+        for i in range(len(lst)-1):
+            if lst[i] > lst[i + 1]:
+                is_exchanged = True
+                swap(lst, i, i + 1)
+            top -= 1
+
+lst = [ 1 , 5 , 2 , 4 , 3 ]
+bubble_sort(lst)
+print(lst)
 
       
   
